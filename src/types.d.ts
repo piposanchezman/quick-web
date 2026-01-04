db153026f6540bcba189ce908850d84ce3e653d7
Andres Felipe Sanchez Manjarres <piposanchezman@gmail.com>
2026-01-03 20:52:06 -0500

    feat: enhance multi-language support with new translations and global accessibility

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,5 +4,10 @@ declare global {
     interface Window {
         toastTimeout?: number;
         showToast?: (message: string) => void;
+        translations?: {
+            [key: string]: {
+                [key: string]: string;
+            };
+        };
     }
 }
